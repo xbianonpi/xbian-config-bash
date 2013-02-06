402224627c1430e5f5092562ab126fa39b01eb06
CurlyMoo <development@xbian.org>
2013-02-06 14:21:21 +0100

    Fixed some bugs

--- etc/bash_completion.d/xbian-config.sh
+++ etc/bash_completion.d/xbian-config.sh
@@ -5,7 +5,7 @@ _xbian-config()
 	cur="${COMP_WORDS[COMP_CWORD]}"
 	prev="${COMP_WORDS[COMP_CWORD-1]}"
 	if [ -d "/usr/local/include/xbian-config/modules/${prev}" ]; then
-		arguments=$(grep ARGUMENTS "/usr/local/include/xbian-config1/modules/${prev}/main" | sed -ne 's/\(.*\)\((\)\(.*\)\().*\)/\3/p');
+		arguments=$(grep ARGUMENTS "/usr/local/include/xbian-config/modules/${prev}/main" | sed -ne 's/\(.*\)\((\)\(.*\)\().*\)/\3/p');
 		COMPREPLY=($(compgen -W "${arguments}" -- $cur))
 		return 0;
 	else
